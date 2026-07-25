03d4635165a7244de9ca2b85c7484abde73e1657
AmairaniCT <tlecuitldel2002@gmail.com>
2026-07-24 21:43:07 -0600

    cambios

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,11 @@
-import { flatRoutes } from "@react-router/fs-routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default flatRoutes();
+export default [
+  index("routes/_index/route.tsx"),
+
+  route("auth/*", "routes/auth.$.tsx"),
+  route("webhooks/app/scopes_update", "routes/webhooks.app.scopes_update.tsx"),
+  route("webhooks/app/uninstalled", "routes/webhooks.app.uninstalled.tsx"),
+
+  route("app/proxy/purchase-order", "routes/app.proxy.purchase-order.tsx"),
+] satisfies RouteConfig;
